b3abda6101b3a6e2a6b2064b59599f2d8bdef70b
sunyongshuai <sunyongshuai@outlook.com>
2019-01-10 16:53:52 +0800

    Add my-alias file and modified my theme and enable some featrues

--- myzsh/zshrc.sh
+++ myzsh/zshrc.sh
@@ -2,6 +2,7 @@
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 # export my environment
 source $HOME/.myrc.sh
+source $HOME/.bash_aliases
 
 # Path to your oh-my-zsh installation.
 export ZSH=$HOME/.oh-my-zsh
@@ -23,10 +24,10 @@ ZSH_THEME="sunicyosen"
 
 # Uncomment the following line to use hyphen-insensitive completion.
 # Case-sensitive completion must be off. _ and - will be interchangeable.
-# HYPHEN_INSENSITIVE="true"
+HYPHEN_INSENSITIVE="true"
 
 # Uncomment the following line to disable bi-weekly auto-update checks.
-# DISABLE_AUTO_UPDATE="true"
+DISABLE_AUTO_UPDATE="true"
 
 # Uncomment the following line to change how often to auto-update (in days).
 # export UPDATE_ZSH_DAYS=13
@@ -38,10 +39,10 @@ ZSH_THEME="sunicyosen"
 # DISABLE_AUTO_TITLE="true"
 
 # Uncomment the following line to enable command auto-correction.
-# ENABLE_CORRECTION="true"
+ENABLE_CORRECTION="true"
 
 # Uncomment the following line to display red dots whilst waiting for completion.
-# COMPLETION_WAITING_DOTS="true"
+COMPLETION_WAITING_DOTS="true"
 
 # Uncomment the following line if you want to disable marking untracked files
 # under VCS as dirty. This makes repository status check for large repositories
--- themes/sunicyosen.zsh-theme
+++ themes/sunicyosen.zsh-theme
@@ -1,7 +1,9 @@
-local ret_status="%(?:%{$fg_bold[green]%}$:%{$fg_bold[red]%}$)"
-PROMPT='${ret_status} %{$fg[cyan]%}%c%{$reset_color%} $(git_prompt_info)'
+local ret_status="%(?:%{$fg_bold[green]%}<:%{$fg_bold[red]%}<)"
+local ret_suf_status="%(?:%{$fg_bold[green]%}>:%{$fg_bold[red]%}>)"
+PROMPT='$(git_prompt_info)${ret_status} %{$fg[cyan]%}%c ${ret_suf_status} %{$reset_color%}'
+#'c' is current directory and 'd' is absolute directory
 
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}git:(%{$fg[red]%}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}[git:(%{$fg[red]%}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}%{$fg_bold[blue]%} ]"
 ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}✗"
 ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%})"
