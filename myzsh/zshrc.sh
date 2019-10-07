480159056ee946a3b4e9ae5fa8b90ca2ab5a7d1a
sunicyosen <sunicyosen@outlook.com>
2019-10-07 19:09:29 +0800

    Modified my zsh

--- myzsh/zshrc.sh
+++ myzsh/zshrc.sh
@@ -66,7 +66,6 @@ ENABLE_CORRECTION="true"
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
 plugins=(
-  autojump
   git
   osx
   sbt
