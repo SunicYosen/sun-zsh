cd2f575d68f1caf165bc5f061549cc3e8bd2bf13
sunicyosen <sunicyosen@outlook.com>
2020-06-28 20:05:27 +0800

    Add tmprc to sunic zsh

---
+++ sunic-zsh/tmprc.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+# export CROSS_COMPILE=arm-linux-gnueabihf-
+# export ARCH=arm
+# export PROJECT_DIR=/home/sun/File/Projects/zybo_z7/linux
+# export KBUILD_DIR=$PROJECT_DIR/linux-xlnx-xilinx-v2019.1
+# export PATH=$PATH:$PROJECT_DIR/u-boot-xlnx/tools
+export VTA_PYNQ_RPC_HOST=192.168.100.10
+export VTA_PYNQ_RPC_PORT=9091
+export VTA_RPC_HOST=192.168.100.10
+export VTA_RPC_PORT=9091
